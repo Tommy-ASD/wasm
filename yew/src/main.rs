0c8fb8c583708f27badb43838dc6dd31e80b506b
Tommy-ASD <82094450+Tommy-ASD@users.noreply.github.com>
2022-06-16 15:17:51 +0200

    sample file from website

--- yew/src/main.rs
+++ yew/src/main.rs
@@ -1,31 +1,55 @@
 use yew::prelude::*;
 
-struct Model {
-    value: i64
+enum Msg {
+    AddOne,
+    ChText,
 }
 
-#[function_component(App)]
-fn app() -> Html {
-    let state = use_state(|| Model { value: 0 });
-    let onclick = {
-        let state = state.clone();
+struct Model {
+    value: i64,
+    on_input_test: String,
+}
 
-        Callback::from(move |_| {
-            // state.set(|s| s.value += 1);
-            state.set(Model { value: state.value + 1 });
-        })
-    };
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
 
-    html! {
-        <div>
-            <button {onclick}>{"+1"}</button>
-            // defines a new paragraph element
-            // the text inside the p element is the value of the state
-            <p>{ &format!("{}", state.value) }</p>
-        </div>
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            value: 0,
+            on_input_test: String::from("Hello, world!"),
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+            Msg::ChText => {
+                self.on_input_test = String::from("inputed");
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
+        html! {
+            <div>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <p>{ self.value }</p>
+                <input type="text" value="he he he haw" oninput={link.callback(|_| Msg::ChText)} />
+                <p>{ &self.on_input_test }</p>
+            </div>
+        }
     }
 }
 
 fn main() {
-    yew::start_app::<App>();
+    yew::start_app::<Model>();
 }
